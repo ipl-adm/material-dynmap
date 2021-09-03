7866b40e7719e5cd033a41b8ed14a6d9ee8a0867
SNDST00M: M.U.N.I.N <82655227+SNDST00M@users.noreply.github.com>
2021-09-03 14:57:29 +0100

    Remove dependencies in Java plugin (#84)

--- java/src/main/java/io/github/sndst00m/MaterialDynmapMain.java
+++ java/src/main/java/io/github/sndst00m/MaterialDynmapMain.java
@@ -1,25 +1,14 @@
 package io.github.sndst00m;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.net.URL;
-import java.net.URLConnection;
-
-import com.google.common.base.Charsets;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.io.ByteSink;
-import com.google.common.io.Files;
-import com.google.gson.JsonParser;
-import com.google.gson.JsonObject;
-import com.google.template.soy.jbcsrc.api.SoySauce;
-import com.google.template.soy.jbcsrc.api.SoySauceBuilder;
 
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
@@ -36,10 +25,10 @@ public class MaterialDynmapMain extends JavaPlugin {
     Plugin dynmap;
     DynmapAPI api;
     FileConfiguration cfg;
-    
+
     int updates_per_tick = 20;
     int vupdates_per_tick = 20;
-    
+
     HashMap<String, Integer> lookup_cache = new HashMap<String, Integer>();
     HashMap<String, Integer> vlookup_cache = new HashMap<String, Integer>();
 
@@ -47,15 +36,15 @@ public class MaterialDynmapMain extends JavaPlugin {
     public void onLoad() {
         log = this.getLogger();
     }
-    
+
     public static void info(String msg) {
         log.log(Level.INFO, msg);
     }
     public static void severe(String msg) {
         log.log(Level.SEVERE, msg);
     }
-    
-    private class OurServerListener implements Listener {
+
+    private class MaterialServerListener implements Listener {
         @EventHandler(priority=EventPriority.MONITOR)
         public void onPluginEnable(PluginEnableEvent event) {
             Plugin p = event.getPlugin();
@@ -65,7 +54,7 @@ public class MaterialDynmapMain extends JavaPlugin {
             }
         }
     }
-    
+
     public void onEnable() {
         info("initializing");
         PluginManager pm = getServer().getPluginManager();
@@ -75,9 +64,8 @@ public class MaterialDynmapMain extends JavaPlugin {
             severe("Cannot find dynmap!");
             return;
         }
-        api = (DynmapAPI)dynmap; /* Get API */
 
-        getServer().getPluginManager().registerEvents(new OurServerListener(), this);        
+        getServer().getPluginManager().registerEvents(new MaterialServerListener(), this);        
 
         /* If enabled, activate */
         if(dynmap.isEnabled())
@@ -85,39 +73,32 @@ public class MaterialDynmapMain extends JavaPlugin {
     }
 
     private void activate() {
-		String jarVersion = this.getDescription().getVersion();
+		String version = this.getDescription().getVersion();
 
 		try {
-			Path dynmapPath = Paths.get(dynmap.getDataFolder().toURI());
-			File dynmapWebIndex = new File(dynmapPath.resolve("web/index.html").toString());
-			String content = Files.toString(dynmapWebIndex, Charsets.UTF_8);
+			Path dynmapWebPath = Paths.get(dynmap.getDataFolder().toURI()).resolve("web/index.html");
+			String content = new String(Files.readAllBytes(dynmapWebPath));;
 
-            if (content.indexOf("material-dynmap") != -1) {
-				info("Material Dynmap v" + jarVersion + " already installed");
+			InputStream injectionStream = this.getResource("injection.html");
+			Scanner s = new Scanner(injectionStream).useDelimiter("\\A");
+			String injection = s.hasNext() ? s.next() : "";
+			injectionStream.close();
+			s.close();
+
+            if (content.indexOf("id=\"material-dynmap-script\"") != -1) {
+				content = content.replaceFirst("<script[^>]+?id=\"material-dynmap-script\"[^>]+?></script>", injection);
+				info("Material Dynmap installation succeeded - updated to v" + version);
 				return;
             }
 
-			URL registry = new URL("https://registry.npmjs.org/material-dynmap");
-			URLConnection request = registry.openConnection();
-			request.connect();
-			JsonParser jp = new JsonParser();
-			JsonObject obj = jp.parse(new InputStreamReader((InputStream) request.getContent())).getAsJsonObject();
-			String version = obj.getAsJsonObject("dist_tag").get("latest").getAsString();
-
-			SoySauce soySauce = new SoySauceBuilder().build();
-			SoySauce.Renderer renderer = soySauce.renderTemplate("io.github.sndst00m.MaterialDynmap");
-			renderer = renderer.setData(ImmutableMap.of("version", version));
-			String injection = renderer.renderText().get();
-
-            content = content.replaceFirst("(?=</head>)", injection + "\n\n");
-			ByteSink sink = Files.asByteSink(dynmapWebIndex);
-			sink.write(content.getBytes());
+			content = content.replaceFirst("(?=</head>)", "\t" + injection + "\n");
+			Files.write(dynmapWebPath, content.getBytes());
         } catch (IOException e) {
             //Simple exception handling, replace with what's necessary for your use case!
-            throw new RuntimeException("Material Dynmap v" + jarVersion + " installation failed", e);
+            throw new RuntimeException("Material Dynmap v" + version + " installation failed", e);
         }
 
-        info("Material Dynmap v" + jarVersion + " installation succeeded");
+        info("Material Dynmap v" + version + " installation succeeded");
     }
 
     public void onDisable() {
