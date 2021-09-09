2820a1e994c2f1438fe0dc4f7dd310a0fce95810
SNDST00M: M.U.N.I.N <82655227+SNDST00M@users.noreply.github.com>
2021-09-09 17:48:05 +0100

    Rewrite Mocha test script to avoid async

--- test/node.test.js
+++ test/node.test.js
@@ -1,49 +1,49 @@
 /* eslint-env node, mocha */
-const { remote } = require("webdriverio");
+const webdriverio = require("webdriverio");
 const assert = require("assert");
 
-const scriptStagingDomain = "https://cdn.jsdelivr.net/gh/SNDST00M/material-dynmap@v0.8.1";
-const userScriptUrl = `${scriptStagingDomain}/src/user.js`;
-const electronScriptUrl = `${scriptStagingDomain}/src/electron.js`;
+webdriverio.remote({
+	capabilities: {
+		browserName: "chrome",
+		pageloadStrategy: this.loadStrategy
+	}
+}).then(function(browser) {
+	const scriptStagingDomain = "https://cdn.jsdelivr.net/gh/SNDST00M/material-dynmap@v0.8.1";
+	const userScriptUrl = `${scriptStagingDomain}/src/user.js`;
+	const electronScriptUrl = `${scriptStagingDomain}/src/electron.js`;
 
-suite("Userscript Tests", async function() {
-	this.timeout(20000);
-	test("./src/user.js", scriptTest.bind({
-		loadStrategy: "eager",
-		resourceUrl: userScriptUrl
-	}));
-	test("./src/electron.js", scriptTest.bind({
-		loadStrategy: "none",
-		resourceUrl: electronScriptUrl
-	}));
-});
-
-/**
- * @param {Mocha.Done} done
- */
-async function scriptTest() {
-	const browser = await remote({
-		capabilities: {
-			browserName: "chrome",
-			pageloadStrategy: this.loadStrategy
-		}
+	suite("Userscript Tests", function() {
+		this.timeout(20000);
+		test("./src/user.js", scriptTest.bind({
+			loadStrategy: "eager",
+			resourceUrl: userScriptUrl
+		}));
+		test("./src/electron.js", scriptTest.bind({
+			loadStrategy: "none",
+			resourceUrl: electronScriptUrl
+		}));
 	});
 
-	await browser.url(process.env.CI_ADDRESS);
-	await browser.execute(`;(function() {
-		const promise = new Promise();
-		window.document.addEventListener("material-dynmap.load", promise.resolve.bind.promise);
-		setTimeout(promise.reject.bind.promise, 5000);
-		import("${userScriptUrl}");
-		return promise;
-	}())`.replace(/^\t/g, ""));
-
-	const sidebarBackgroundColor = await $(".sidebar").getCSSProperty("background-color");
-	assert.strictEqual(sidebarBackgroundColor, "rgb(33, 33, 33)");
-	const chipContainer = await $(".leaflet-chip-container");
-	assert(chipContainer.isExisting(), "Chip container does not exist!");
-	const typeContainer = await $(".leaflet-type-container");
-	assert(typeContainer.isExisting(), "Type container does not exist!");
-
-	return;
-}
+	/**
+	 * @param {Mocha.Done} done
+	 */
+	function scriptTest(done) {
+		browser.url(process.env.CI_ADDRESS).then(function() {
+			browser.execute(`;(function() {
+				const promise = new Promise();
+				window.document.addEventListener("material-dynmap.load", promise.resolve.bind.promise);
+				setTimeout(promise.reject.bind.promise, 5000);
+				import("${userScriptUrl}");
+				return promise;
+			}())`.replace(/^\t\t/g, "")).then(async function() {
+				const sidebarBackgroundColor = await $(".sidebar").getCSSProperty("background-color");
+				assert.strictEqual(sidebarBackgroundColor, "rgb(33, 33, 33)");
+				const chipContainer = await $(".leaflet-chip-container");
+				assert(chipContainer.isExisting(), "Chip container does not exist!");
+				const typeContainer = await $(".leaflet-type-container");
+				assert(typeContainer.isExisting(), "Type container does not exist!");
+				done();
+			});
+		});
+	}
+});
